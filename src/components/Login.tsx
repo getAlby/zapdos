d33881c162fd54b5facef710c8ea487902923aed
escapedcat <github@htmlcss.de>
2022-10-20 18:01:41 +0200

    feat: further stylign and marquee

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,34 @@
-import pkceChallenge from 'pkce-challenge'
-import {Config} from './helpers'
+import pkceChallenge from "pkce-challenge";
+import { Config } from "./helpers";
 const Login = () => {
+  const challenge = pkceChallenge();
+  window.sessionStorage.setItem("code_verifier", challenge.code_verifier);
 
-	const challenge = pkceChallenge()
-	window.sessionStorage.setItem("code_verifier", challenge.code_verifier);
-
-	return (
-		<div>
-			<a className="btn" href={Config.appHost
-				+ "?client_id="+ Config.clientId 
-				+ "&redirect_uri=" + Config.redirectUri
-				+ "&scope=" + Config.scope
-				+ "&code_challenge=" + challenge.code_challenge
-				+ "&code_challenge_method=S256"
-				}>
-				🐝 Connect your Alby account</a>
-		</div>
-	)
-}
+  return (
+    <div>
+      <a
+        className="btn"
+        href={
+          Config.appHost +
+          "?client_id=" +
+          Config.clientId +
+          "&redirect_uri=" +
+          Config.redirectUri +
+          "&scope=" +
+          Config.scope +
+          "&code_challenge=" +
+          challenge.code_challenge +
+          "&code_challenge_method=S256"
+        }
+      >
+        <img
+          src="https://getalby.com/website/_assets/logo-WIC6GJUP.svg"
+          alt="Alby logo"
+        />{" "}
+        <span>Connect your Alby account</span>
+      </a>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,61 +1,70 @@
 import ListMessage from "./ListMessage/ListMessage";
 import React, { useState } from "react";
-import {BuySellTransaction, Transfer, TYPE_TRANSFER} from "../helpers";
-import './style.css';
+import { BuySellTransaction, Transfer, TYPE_TRANSFER } from "../helpers";
+import "./style.css";
 import { useInterval } from "../../helpers";
-import {Config} from '../helpers'
+import { Config } from "../helpers";
 
 interface Props {
-    title: string;
-    transfers: Transfer[] | BuySellTransaction[];
+  title: string;
+  transfers: Transfer[] | BuySellTransaction[];
 }
 
-const CELLS_MAX_COUNT = 30;
-const API_URL = Config.apiHost
+const CELLS_MAX_COUNT = 10;
+const API_URL = Config.apiHost;
 const query = window.location.search;
 const params = new URLSearchParams(query);
-const accessToken = params.get("access_token")
+const accessToken = params.get("access_token");
 
 // Parameters for displaying
 const POLLING_INTERVAL = 3000;
 
 const MessageList: React.FC<Props> = ({}) => {
+  const [transfers, setTransfers] = useState<any>([]);
+  const [lastId, setLastId] = useState(new Date().toISOString());
 
-const [ transfers, setTransfers ] = useState<any>([]);
-const [ lastId, setLastId ] = useState(new Date().toISOString());
+  useInterval(() => {
+    fetch(API_URL + "/invoices/incoming", {
+      method: "get",
+      headers: { Authorization: accessToken! },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // filter only new transactions for target user
+        const newUserTransactions = data
+          .filter((transaction: Transfer) => transaction.settled_at > lastId)
+          .map((transaction: Transfer) => ({
+            ...transaction,
+            type: TYPE_TRANSFER,
+          }))
+          .map((transaction: Transfer) => ({
+            ...transaction,
+            payer_name:
+              transaction.payer_name == null
+                ? "anonymous"
+                : transaction.payer_name,
+          }));
+        if (newUserTransactions.length) {
+          setTransfers((prevTransfers: any) => [
+            ...newUserTransactions,
+            ...prevTransfers,
+          ]);
+          setLastId(newUserTransactions[0].settled_at);
+        }
+      })
+      .catch((e) => console.log(e));
+  }, POLLING_INTERVAL);
 
-    useInterval(() => {
-        fetch(API_URL+'/invoices/incoming', { method: 'get' , headers: {'Authorization': accessToken!}})
-            .then(res => res.json())
-            .then(data => {
-                // filter only new transactions for target user
-                const newUserTransactions = data
-                    .filter((transaction: Transfer) => transaction.settled_at > lastId)
-                    .map((transaction: Transfer) => ({ ...transaction, type: TYPE_TRANSFER }))
-                    .map((transaction: Transfer) => ({ ...transaction, payer_name: transaction.payer_name == null ? 'anonymous': transaction.payer_name}));
-                if (newUserTransactions.length) {
-                    setTransfers((prevTransfers: any) => [
-                        ...newUserTransactions,
-                        ...prevTransfers
-                    ]);
-                    setLastId(newUserTransactions[0].settled_at);
-                }
-            })
-            .catch(e => console.log(e));
-    }, POLLING_INTERVAL);
-
-    return (
-        <div className="messageList">
-            <div className="messageListContent">
-            {
-                transfers &&
-                transfers.slice(0, CELLS_MAX_COUNT).map((transaction: any) => {
-                    return <ListMessage transaction={transaction} />;
-                })
-            }
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="messageList">
+      <div className="messageListContent">
+        {transfers &&
+          transfers.slice(0, CELLS_MAX_COUNT).map((transaction: any) => {
+            return <ListMessage transaction={transaction} />;
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default MessageList;
