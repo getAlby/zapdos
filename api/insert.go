263433265e6c139ce2fc853a24982f0076ff4b62
kiwiidb <kwinten.debacker@hotmail.com>
2022-10-25 12:58:32 +0200

    add insert, delete

---
+++ api/insert.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type BlockListItem struct {
+	PaymentID string `json:"payment_id"`
+	UserID    string `json:"user_id"`
+}
+
+func InsertHandler(w http.ResponseWriter, r *http.Request) {
+	userToken := r.Header.Get("Authorization")
+	paymentId := r.URL.Query().Get("payment_id")
+	supaClient.DB.From("zapdos").Insert(&BlockListItem{
+		PaymentID: paymentId,
+		UserID:    userToken,
+	}).Execute(nil)
+	json.NewEncoder(w).Encode(&BlockListItem{
+		PaymentID: paymentId,
+	})
+}
