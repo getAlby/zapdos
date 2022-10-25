3ad6ea60e5cc020761dab03d01aab5bd964dc406
kiwiidb <kwinten.debacker@hotmail.com>
2022-10-25 11:58:14 +0200

    debug api

--- api/list.go
+++ api/list.go
@@ -12,7 +12,7 @@ import (
 var supaClient *supa.Client
 
 func ListHandler(w http.ResponseWriter, r *http.Request) {
-	var result map[string]interface{}
+	var result []map[string]interface{}
 	err := supaClient.DB.From("zapdos").Select("*").Execute(&result)
 	if err != nil {
 		fmt.Printf("something went wrong: %s \n", err.Error())
