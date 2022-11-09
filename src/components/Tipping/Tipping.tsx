5d14b12d633ed5caea4bd005049b4cb286e4c4da
escapedcat <github@htmlcss.de>
2022-11-09 12:39:45 +0100

    feat: optimize for adoptingbitcoin@getalby.com

--- src/components/Tipping/Tipping.tsx
+++ src/components/Tipping/Tipping.tsx
@@ -27,7 +27,7 @@ const Tipping: React.FC = () => {
     <>
       {lnurl && (
         <div className="tipping">
-          <div className="mt-2">⚡{lnurl}</div>
+          <div className="mb-2">⚡{lnurl}</div>
           <QRCode value={"lightning:" + lnurl} size={200} />
           <div className="mt-2">
             Minimum tip amount: <br />
